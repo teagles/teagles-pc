8c2c95f8ba16c6e9d977f0fb0412a05c73be4671
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-13 22:34:12 +1000

    Problem 00 solved.

--- 00/00.py
+++ 00/00.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 import sys
+import math
 
 URL_PREFIX = 'http://www.pythonchallenge.com/pc/def/'
 URL_SUFFIX = '.html'
 START = 0
 
 
+# 00/00.py 2 38
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
-    print 'URL is: %s%d%s' % (URL_PREFIX, START, URL_SUFFIX)
+    print 'URL is: %s%d%s' % (URL_PREFIX, math.pow(int(args[0]), int(args[1])),
+                              URL_SUFFIX)
 
 
 if __name__ == '__main__':
