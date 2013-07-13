8cd98fe4b993845b956cbca7c6782f23bf4a7340
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-13 23:05:10 +1000

    Problem 02 start.

---
+++ 02/02.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#http://www.pythonchallenge.com/pc/def/ocr.html
+import sys
+
+
+# 02/02.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
