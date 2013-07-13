beb7bc5c38ddaf6010b8469cd147bc8f95ab2c6d
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-13 23:00:09 +1000

    Problem 01 solution.

--- 01/01.py
+++ 01/01.py
@@ -2,15 +2,20 @@
 #http://www.pythonchallenge.com/pc/def/map.html
 import sys
 import math
+from string import maketrans
+
+INCHARS = 'abcdefghijklmnopqrstuvwxyz'
+OUTCHARS = 'cdefghijklmnopqrstuvwxyzab'
 
 
 # 01/01.py "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc
 # dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle.
 # sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+# 01/01.py map
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
-    print args[0]
+    print args[0].translate(maketrans(INCHARS, OUTCHARS))
 
 
 if __name__ == '__main__':
