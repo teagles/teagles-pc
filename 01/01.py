ab6844511683cc8d5710630265f9f7b47e5c772a
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-13 22:42:59 +1000

    Problem 01 start.

---
+++ 01/01.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#http://www.pythonchallenge.com/pc/def/map.html
+import sys
+import math
+
+
+# 01/01.py "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc
+# dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle.
+# sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+    print args[0]
+
+
+if __name__ == '__main__':
+    main()
