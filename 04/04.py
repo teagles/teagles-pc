4c7ce344ccc2aa8924be035c2db1f5230e3c4883
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-20 20:52:38 +1000

    Problem 04 solution.

--- 04/04.py
+++ 04/04.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
 #http://www.pythonchallenge.com/pc/def/linkedlist.php
 import sys
-import string
 import re
 import urllib2
 
-SITE = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345"
+SITE = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=%s"
+MATCHER = re.compile("and the next nothing is (\d+)")
 
 
-# 04/04.py
+def getPage(nothing):
+    print SITE % nothing
+    return urllib2.urlopen(SITE % nothing).read()
+
+
+# 04/04.py 12345
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
+    text = getPage(args[0])
+    match = MATCHER.findall(text)
+    while len(match) > 0:
+        text = getPage(match[0])
+        match = MATCHER.findall(text)
+    print text
 
 
 if __name__ == '__main__':
