7b136db9fcc5b1ee08158f70dd796808ebe287c7
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-20 19:22:54 +1000

    Problem 04 start.

---
+++ 04/04.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#http://www.pythonchallenge.com/pc/def/linkedlist.php
+import sys
+import string
+import re
+import urllib2
+
+SITE = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345"
+
+
+# 04/04.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
