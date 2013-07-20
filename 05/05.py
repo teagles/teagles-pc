1c5e7c6373f4fe1112d334f92d4f79d2a543316b
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-20 20:55:57 +1000

    Problem 05 start.

---
+++ 05/05.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#http://www.pythonchallenge.com/pc/def/peak.html
+import sys
+
+
+# 05/05.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
