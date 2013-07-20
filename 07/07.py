66246a8c00902cdfadb0b2e84fa701ae0f52aa13
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-20 22:04:42 +1000

    Problem 07 start.

---
+++ 07/07.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#http://www.pythonchallenge.com/pc/def/oxygen.html
+import sys
+
+
+# 07/07.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
