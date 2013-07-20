0569b54cf202392bb3b239a0623af62b5de3c1f7
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-20 16:06:08 +1000

    Problem 02 solution.

--- 02/02.py
+++ 02/02.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 #http://www.pythonchallenge.com/pc/def/ocr.html
 import sys
+import urllib
+
+SITE = "http://www.pythonchallenge.com/pc/def/ocr.html"
+
+
+def isChar(x):
+    if x.isalpha():
+        return x
+    else:
+        return ''
 
 
 # 02/02.py
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
+    print ''.join(map(isChar, urllib.urlopen(SITE).read()))
 
 
 if __name__ == '__main__':
