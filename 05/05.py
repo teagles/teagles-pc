da942cfd1f68362b8057a0dd3ceb97c9e8f0ab51
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-20 21:21:19 +1000

    Problem 05 solution.

--- 05/05.py
+++ 05/05.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
 #http://www.pythonchallenge.com/pc/def/peak.html
 import sys
+import urllib2
+import pickle
+
+PICKLE_FILE = "http://www.pythonchallenge.com/pc/def/banner.p"
+
+
+def printMatrix(matrix):
+    for line in matrix:
+        for char, num in line:
+            for x in range(0, num):
+                sys.stdout.write(char)
+        sys.stdout.write('\n')
 
 
 # 05/05.py
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
+    p = urllib2.urlopen(PICKLE_FILE).read()
+    matrix = pickle.loads(p)
+    printMatrix(matrix)
 
 
 if __name__ == '__main__':
