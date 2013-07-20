99448b693304e0bd2e0e806e9ca5bfbc7adc296e
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-20 16:07:48 +1000

    Problem 03 start.

---
+++ 03/03.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#http://www.pythonchallenge.com/pc/def/equality.html
+import sys
+
+
+# 03/03.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
