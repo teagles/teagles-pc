bb838048c5f1e9383b3df585a78da555795ea840
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-21 21:45:11 +1000

    Problem 08 start.

---
+++ 08/08.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#http://www.pythonchallenge.com/pc/def/integrity.html
+import sys
+
+
+# 08/08.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
