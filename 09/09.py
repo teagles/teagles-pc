ae09f70aa71f77771aeab47df10ab5b9e3b43182
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-22 19:28:32 +1000

    Problem 09 start.

---
+++ 09/09.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#http://huge:file@www.pythonchallenge.com/pc/return/good.html
+import sys
+
+
+# 09/09.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
