69859e192e8413c1dfa97a39d5a5befcdf9fbb5e
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-22 21:16:45 +1000

    Problem 11 start.

---
+++ 11/11.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# http://huge:file@www.pythonchallenge.com/pc/return/5808.html
+import sys
+
+
+# 11/11.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
