bf07871fe8af92126de87bd02ca5b5866a067671
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-22 21:15:16 +1000

    Problem 10 solution.

--- 10/10.py
+++ 10/10.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python
 # http://huge:file@www.pythonchallenge.com/pc/return/bull.html
 import sys
+import itertools
+
+
+def groups(str):
+    return [list(g) for k, g in itertools.groupby(str)]
+
+
+def newTokens(str):
+    for group in groups(str):
+        yield '%i%s' % (len(group), group[0])
+
+
+def seeNSay(str):
+    return ''.join(newTokens(str))
+
+
+def series(num):
+    str = "1"
+    for i in range(0, num):
+        str = seeNSay(str)
+    return str
 
 
 # 10/10.py
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
+    print len(series(30))
 
 
 if __name__ == '__main__':
