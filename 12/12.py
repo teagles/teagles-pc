db3954e9c05552f24ac7d58e6233a426ba24cb05
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-22 21:39:45 +1000

    Problem 12 start.

---
+++ 12/12.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# http://huge:file@www.pythonchallenge.com/pc/return/evil.html
+import sys
+
+
+# 12/12.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
