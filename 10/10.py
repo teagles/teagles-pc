b1519b8d3a9915dbd8be3e2cffe138d0210be7dd
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-22 20:16:53 +1000

    Problem 10 start.

---
+++ 10/10.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# http://huge:file@www.pythonchallenge.com/pc/return/bull.html
+import sys
+
+
+# 10/10.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
