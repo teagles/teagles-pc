c895d9df2b027cbf104b0634ca7c46a59202f640
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-25 21:29:34 +1000

    Problem 14 start.

---
+++ 14/14.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# http://huge:file@www.pythonchallenge.com/pc/return/italy.html
+import sys
+
+
+# 14/14.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
