6c343558ab4311397ace1b03707c7eb131c71500
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-25 19:51:35 +1000

    Problem 13 start.

---
+++ 13/13.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# http://huge:file@www.pythonchallenge.com/pc/return/disproportional.html
+import sys
+
+
+# 13/13.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
