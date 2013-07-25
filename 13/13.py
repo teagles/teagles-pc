ce815c6959738f41465e1b1cfa0315ede9e23c0a
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-25 21:28:17 +1000

    Problem 13 solution.

--- 13/13.py
+++ 13/13.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 # http://huge:file@www.pythonchallenge.com/pc/return/disproportional.html
 import sys
+import xmlrpclib
+
+URL = 'http://www.pythonchallenge.com/pc/phonebook.php'
 
 
-# 13/13.py
+# 13/13.py Bert
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
+    s = xmlrpclib.ServerProxy(URL)
+    print s.phone(args[0])
 
 
 if __name__ == '__main__':
