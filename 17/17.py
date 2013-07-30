84b1376d67ee2e3df1ca4df533cea13722a5d0e5
Tim Eagles <tim.eagles.au@gmail.com>
2013-07-30 20:05:48 +1000

    Problem 17 start.

---
+++ 17/17.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# http://huge:file@www.pythonchallenge.com/pc/return/romance.html
+import sys
+
+
+# 17/17.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
