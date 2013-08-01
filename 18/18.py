216bfb2d0e7ba24413328bc59a96203ea7b0fa7c
Tim Eagles <tim.eagles.au@gmail.com>
2013-08-01 12:34:26 +1000

    Problem 18 start.

---
+++ 18/18.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# http://huge:file@www.pythonchallenge.com/pc/return/balloons.html
+import sys
+
+# 18/18.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
