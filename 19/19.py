481288df40b5e3bc1d901fcb4ff0abe8c4f899c5
Tim Eagles <tim.eagles.au@gmail.com>
2013-08-03 21:50:12 +1000

    Problem 19 start.

---
+++ 19/19.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# http://butter:fly@www.pythonchallenge.com/pc/hex/bin.html
+import sys
+
+
+# 19/19.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
