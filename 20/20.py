0a8564ea9d1fb4c27363975ace4a45c04ba39f1a
Tim Eagles <tim.eagles.au@gmail.com>
2014-04-25 15:50:25 +1000

    Problem 20 start.

---
+++ 20/20.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# http://butter:fly@www.pythonchallenge.com/pc/hex/idiot2.html
+import sys
+
+
+# 20/20.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
