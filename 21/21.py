89754fe41f8d0c6b34da05605b4e8b8585a83b94
Tim Eagles <tim.eagles.au@gmail.com>
2014-05-12 07:02:55 +1000

    21 boilerplate.

---
+++ 21/21.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# http://butter:fly@www.pythonchallenge.com/pc/hex/unreal.jpg
+# 'Range': 'bytes=1152983631-30347'
+# Password: redavni
+import sys
+
+ZLIB_HEAD = 'x\x9c'
+
+
+# 21/21.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
