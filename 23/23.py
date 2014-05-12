0d55fa96df242b710bd81218260cd8a6cfb6b6fe
Tim Eagles <tim.eagles.au@gmail.com>
2014-05-13 07:27:54 +1000

    Problem 23 start.

---
+++ 23/23.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# http://butter:fly@www.pythonchallenge.com/pc/hex/bonus.html
+import sys
+
+
+# 23/23.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
