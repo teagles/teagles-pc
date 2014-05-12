7d5e7a1756b04b12921fe8c56493eba329f0a4c3
Tim Eagles <tim.eagles.au@gmail.com>
2014-05-12 12:39:22 +1000

    Problem 22 start.

---
+++ 22/22.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# http://butter:fly@www.pythonchallenge.com/pc/hex/copper.html
+import sys
+
+
+# 22/22.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
