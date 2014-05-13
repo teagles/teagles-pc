cffbf544e83e55101a375f19bcebe3de6ed9c51d
Tim Eagles <tim.eagles.au@gmail.com>
2014-05-13 22:03:49 +1000

    Problem 23 solution.

--- 23/23.py
+++ 23/23.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 # http://butter:fly@www.pythonchallenge.com/pc/hex/bonus.html
-import sys
+import this
 
-
-# 23/23.py
-def main(args=None):
-    if args is None:
-        args = sys.argv[1:]
-
-
-if __name__ == '__main__':
-    main()
+print 'va gur snpr bs jung?'.decode('rot13')
