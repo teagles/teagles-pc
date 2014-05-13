e49b66cdcac1f9c8a2bc60e5f558d448f01a96a5
Tim Eagles <tim.eagles.au@gmail.com>
2014-05-13 22:06:04 +1000

    Problem 24 start.

---
+++ 24/24.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# http://butter:fly@www.pythonchallenge.com/pc/hex/ambiguity.html
+import sys
+
+
+# 24/24.py
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+
+if __name__ == '__main__':
+    main()
